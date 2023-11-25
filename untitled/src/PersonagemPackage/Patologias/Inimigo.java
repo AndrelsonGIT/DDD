b6640bf9975ad84cbbe9818e36b884dae11ffa56
AndrelsonGIT <148652838+AndrelsonGIT@users.noreply.github.com>
2023-11-24 22:29:56 -0300

    Apaga métodos e altera construtor da Classe Inimigo

--- untitled/src/PersonagemPackage/Patologias/Inimigo.java
+++ untitled/src/PersonagemPackage/Patologias/Inimigo.java
@@ -5,19 +5,13 @@ import PersonagemPackage.Personagem;
 
 public abstract class Inimigo extends Personagem {
 
-    private int ataque;
-    private int defesa;
-
     protected Inimigo(){
 
     }
 
-    protected Inimigo(String nome, int vida, int defesa) {
-        super(nome, vida);
-        this.defesa = defesa;
+    protected Inimigo(String nome, int vida,int ataque, int defesa) {
+        super(nome, vida, ataque,defesa);
     }
-
-    abstract void ataque(Jogador jogador);
 
     abstract void ataque(Jogador jogador, SuperAtaque superAtaque);
 }
