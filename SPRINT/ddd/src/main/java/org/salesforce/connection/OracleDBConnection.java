0864780e9225f85c3a9512b01856dc8869a5a275
AndrelsonGIT <148652838+AndrelsonGIT@users.noreply.github.com>
2024-04-02 21:44:49 -0300

    update

--- SPRINT/ddd/src/main/java/org/salesforce/connection/OracleDBConnection.java
+++ SPRINT/ddd/src/main/java/org/salesforce/connection/OracleDBConnection.java
@@ -8,7 +8,6 @@ public class OracleDBConnection {
 
     public static Connection getConnection(){
 
-
         try {
             Class.forName("oracle.jdbc.driver.OracleDriver");
             Connection connection = DriverManager.getConnection("jdbc:oracle:thin:@oracle.fiap.com.br:1521:ORCL", "RM552639", "241104");
